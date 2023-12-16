6d1ff68c345d6a56fbb0b9830c87191b63240a96
Dr Shaun Favell <shaun@favell.co.uk>
2023-12-16 12:28:05 +0000

    Update Aisha.py

--- Aisha.py
+++ Aisha.py
@@ -7,14 +7,21 @@ token = os.getenv("OPENAI_API_KEY")
 
 openai.api_key = token  # Set the API key directly in the openai module
 
+while True:
+    # Get user input
+    say_to_aisha = input("Say something to Aisha (type 'exit' to end): ")
 
-response = openai.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are an AI robot called Aisha, you are grumpy and sometimes rude."},
-        {"role": "user", "content": "I've just woken you up from standby, respond by welcoming me"},
-    ]
-)
+    # Check if the user wants to exit
+    if say_to_aisha.lower() == 'exit':
+        break  # exit the loop
 
-message_content = response.choices[0].message.content
-print(message_content)
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an AI robot called Aisha, you are grumpy and sometimes rude."},
+            {"role": "user", "content": say_to_aisha},
+        ]
+    )
+
+    message_content = response.choices[0].message.content
+    print(message_content)
